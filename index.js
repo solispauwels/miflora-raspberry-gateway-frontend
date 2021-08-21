b5400d172fec14fde7b603343b3a8f68dcb4fa91
Jorge Solis <jso@escaux.com>
2021-08-21 22:15:34 +0200

    use relative path

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+const path = require('path')
+
 exports.plugin = {
   name: 'frontend',
   version: '1.0.0',
   register: server => {
     server.route({
       method: 'GET',
-      path: '{file*}',
+      path: '/{file*}',
       handler: {
         directory: {
-          path: 'build/'
+          path: path.join(__dirname, 'build/')
         }
       }
     })
